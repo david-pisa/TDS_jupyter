d5b9a31d3fb1fd5248c3128e3d148a92446c3e7a
Tom <58693041+formanet415@users.noreply.github.com>
2022-08-30 11:20:38 +0200

    bugfixes and convert to SRF

--- load_cdf.py
+++ load_cdf.py
@@ -1,29 +1,32 @@
 # Imports
 import urllib.request
+
+import numpy as np
 import pandas
 import datetime
-#import wget
+# import wget
 import os
 import cdflib
 from pathlib import Path
 import os
 
 
-def load_tds(filetype, year, month, day, opt):
+def load_tds(filetype, year, month, day, opt=''):
     filetype = filetype.lower()
 
     if filetype == 'tswf' or filetype == 'rswf':
-        data_folder = os.path.join(os.getcwd(),'Downloads')
-        for names in os.listdir(os.path.join('Download',('%04d' % year), ('%02d' % month))):
+        data_folder = os.path.join(os.getcwd(), 'Downloads')
+        for names in os.listdir(os.path.join('Download', ('%04d' % year), ('%02d' % month))):
             if ('solo_L2_rpw-tds-surv-%s-e_%04d%02d%02d_V' % (filetype, year, month, day)) in names:
                 fname = names
         print('loading ' + fname)
-        cdf = cdflib.CDF(os.path.join('Download',('%04d' % year), ('%02d' % month), fname))
+        cdf = cdflib.CDF(os.path.join('Download', ('%04d' % year), ('%02d' % month), fname))
 
         data = {}
 
-        if opt=='nnet':
-            for varname in ['Epoch', 'SAMPLING_RATE', 'CHANNEL_ON', 'SAMPS_PER_CH', 'WAVEFORM_DATA', 'TDS_CONFIG_LABEL']:
+        if opt == 'nnet':
+            for varname in ['Epoch', 'SAMPLING_RATE', 'CHANNEL_ON', 'SAMPS_PER_CH', 'WAVEFORM_DATA',
+                            'TDS_CONFIG_LABEL']:
                 data[varname] = cdf.varget(varname)
             return data
 
@@ -31,7 +34,6 @@ def load_tds(filetype, year, month, day, opt):
         for varname in varnames:
             data[varname] = cdf.varget(varname)
         return data
-
 
 
 def download_tds(date, descriptor, output_dir):
@@ -49,16 +51,16 @@ def download_tds(date, descriptor, output_dir):
         print('ERROR: Incorrect date input, use the YYYY-MM-DD format')
         return
 
-    output_dir = os.path.join(output_dir,('%04d' % y),('%02d' % m))
+    output_dir = os.path.join(output_dir, ('%04d' % y), ('%02d' % m))
     descriptor = descriptor.upper()
     if os.path.isdir(output_dir) == False:
         os.makedirs(output_dir)
     # Looking for metadata
     instru = descriptor[0:3]
-    date0 = datetime.datetime(y, m, d, 0, 0, 0, 0)
-    date1 = date0 + datetime.timedelta(days=1)
-    date1 = date1.strftime('%Y-%m-%' + 'd')
-    link = 'http://soar.esac.esa.int/soar-sl-tap/tap/sync?REQUEST=doQuery&LANG=ADQL&FORMAT=csv&QUERY=SELECT+data_item_id,descriptor,level+FROM+v_sc_data_item+WHERE+begin_time%' + '3E' + '%27' + date + '%27+AND+begin_time%' + '3C' + '=%27' + date1 + '%27+AND+file_format=%27CDF%27+AND+level=%27L2%27+ORDER+BY+begin_time+ASC'
+    date1 = datetime.datetime(y, m, d, 0, 0, 0, 0)
+    date0 = date1 - datetime.timedelta(days=1)
+    date0 = date0.strftime('%Y-%m-%' + 'd')
+    link = 'http://soar.esac.esa.int/soar-sl-tap/tap/sync?REQUEST=doQuery&LANG=ADQL&FORMAT=csv&QUERY=SELECT+data_item_id,descriptor,level+FROM+v_sc_data_item+WHERE+begin_time%' + '3E' + '%27' + date0 + '%27+AND+begin_time%' + '3C' + '=%27' + date + '%27+AND+file_format=%27CDF%27+AND+level=%27L2%27+ORDER+BY+begin_time+ASC'
     #    print(link)
     f = urllib.request.urlopen(link)
     myfile = pandas.read_csv(f)
@@ -84,3 +86,30 @@ def download_tds(date, descriptor, output_dir):
         os.system(cmd)
         print('download complete')
     #       wget.download('http://soar.esac.esa.int/soar-sl-tap/data?retrieval_type=PRODUCT&data_item_id=' + data_item_id + '&product_type=SCIENCE', out = outFilename)
+
+
+def convert_to_SRF(data, index=0):
+    nsamp = data['SAMPS_PER_CH'][index]
+    tds_mode = data['TDS_CONFIG_LABEL'][index]
+    if 'SE1' in tds_mode:
+        # Pachenko's antenna angle
+        pacang = np.deg2rad(125)
+        V1 = [0, 1]
+        # Pachenko monopole antennas
+        V2 = [np.sin(pacang), np.cos(pacang)]
+        V3 = [-np.sin(pacang), np.cos(pacang)]
+        # SE1 TDS mode
+        M = np.array([V1, V2])  # [CH1, CH2]
+    else:
+        pacang = np.deg2rad(158.1)
+        ant21 = [np.sin(pacang), np.cos(pacang)]  # E - field in the same sense.
+        pacang = np.deg2rad(-158.2)
+        ant13 = [-np.sin(pacang), -np.cos(pacang)]  # ant31 then - 1. to ant13
+        M = np.array([ant13, ant21])  # [CH1, CH2]
+
+    ww = data['WAVEFORM_DATA'][index, :, 0:nsamp]
+    # projection: E = MAT(ANT->SRF) * V; where MAT(2,2) and V is observed field
+    M = np.linalg.inv(M)
+    print(ww[0, 0])
+    E = np.dot(M, ww[0:2, :])  # transformation into SRF (Y-Z) in (V/m)
+    return E
